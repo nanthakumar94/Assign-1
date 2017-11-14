fcf7d457f35272d0eec0e5d04915b320f87e0ac6
nanthakumar94 <nantha.k1994@gmail.com>
2017-11-14 12:11:42 +0800

    This is aspx file
    this is your first assignment

---
+++ Assign-1.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Assign-1.aspx.cs" Inherits="SodingTest_1.Assign_1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Assign-1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SodingTest_1
+{
+    public partial class Assign_1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            sum();
+        }
+        public void sum()
+        {
+            int i = 1, j = 1, sum = 0;
+            while (i < 4000000)
+            {
+                i = i + j;
+                j = i - j;
+                if (i % 2 == 0)
+                    sum += i;
+            }
+            Response.Write(sum);
+       }
+    }
+}
